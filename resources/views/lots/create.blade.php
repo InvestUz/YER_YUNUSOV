66a6a8361ab8f5172e4ed4fc71110b978afb0ec0
InvestUz <invest.abdusattorov@gmail.com>
2025-10-20 16:30:26 +0500

    upgrading

--- resources/views/lots/create.blade.php
+++ resources/views/lots/create.blade.php
@@ -114,7 +114,7 @@
 
                     <div>
                         <label class="block text-sm font-bold text-gray-900 mb-2">
-                            Тўлиқ манзил <span class="text-red-600">*</span>
+                            Тўлиқ манзил
                         </label>
                         <input type="text" name="address" id="address" required
                             class="w-full px-4 py-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-600 font-medium transition"
